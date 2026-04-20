0e37d724ad452b7efcfe6dddc65c310111cf9734
raviattrash-pro <raviattrash1@gmail.com>
2026-04-20 16:51:58 +0530

    admin account issue

--- Interview_App/backend/src/main/java/com/interviewsim/app/model/AdminAccount.java
+++ Interview_App/backend/src/main/java/com/interviewsim/app/model/AdminAccount.java
@@ -1,15 +1,9 @@
 package com.interviewsim.app.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(name = "admin_accounts")
-@Getter
-@Setter
-@NoArgsConstructor
 public class AdminAccount {
 
     @Id
@@ -22,8 +16,35 @@ public class AdminAccount {
     @Column(nullable = false)
     private String password;
 
+    public AdminAccount() {
+    }
+
     public AdminAccount(String username, String password) {
         this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
     }
 }
